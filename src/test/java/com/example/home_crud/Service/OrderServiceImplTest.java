7e1db4c94074bea4c4070924a81bc869a5dd938e
Veniamin12 <ven4ik11@gmail.com>
2023-12-24 14:54:39 +0200

    3. Add test class for ProductServiceImplTest and OrderServiceImplTest.

---
+++ src/test/java/com/example/home_crud/Service/OrderServiceImplTest.java
@@ -0,0 +1,150 @@
+package com.example.home_crud.Service;
+
+import com.example.home_crud.Converter.OrderConverter;
+import com.example.home_crud.DTO.OrderDto;
+import com.example.home_crud.Model.Order;
+import com.example.home_crud.Repository.order.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class OrderServiceImplTest {
+
+    public static final int ORDER_ID = 1;
+    @InjectMocks
+    private OrderServiceImpl testInstance;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @Mock
+    private OrderConverter orderConverter;
+
+    @Mock
+    private Order order;
+    @Mock
+    private List<Order> orderList;
+
+    private OrderDto orderDto;
+    private List<OrderDto> orderDtoList;
+
+    @BeforeEach
+    public void init() {
+        orderDto = new OrderDto();
+        orderDto.setId(ORDER_ID);
+        orderDto.setCost(7.33);
+        LocalDate specificDate = LocalDate.of(1998, 5, 5);
+        orderDto.setDate(specificDate);
+
+        orderDtoList = new ArrayList<>();
+        orderDtoList.add(orderDto);
+    }
+
+    @Test
+    void shouldGetOrderById() {
+        when(orderRepository.findById(anyInt())).thenReturn(Optional.of(order));
+        when(orderConverter.fromModel(order)).thenReturn(orderDto);
+
+        OrderDto result = testInstance.getOrderById(ORDER_ID);
+
+        verify(orderRepository).findById(ORDER_ID);
+        verify(orderConverter).fromModel(order);
+        assertEquals(ORDER_ID, result.getId());
+        assertNotNull(result);
+    }
+
+    @Test
+    void shouldNotGetOrderById() {
+        assertThrows(NoSuchElementException.class, () -> {
+            testInstance.getOrderById(ORDER_ID);
+        });
+    }
+
+    @Test
+    void shouldGetAllOrders() {
+        when(orderRepository.findAll()).thenReturn(orderList);
+        when(orderConverter.fromModel(orderList)).thenReturn(orderDtoList);
+
+        List<OrderDto> result = testInstance.getAllOrders();
+
+        verify(orderRepository).findAll();
+        verify(orderConverter).fromModel(orderList);
+        assertEquals(orderDtoList, result);
+        assertNotNull(result);
+
+    }
+
+    @Test
+    void shouldNotGetAllOrders() {
+        when(orderRepository.findAll()).thenReturn(null);
+
+        List<OrderDto> result = testInstance.getAllOrders();
+
+        verify(orderRepository).findAll();
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldAddOrder() {
+        when(orderConverter.toModel(orderDto)).thenReturn(order);
+
+        testInstance.addOrder(orderDto);
+
+        verify(orderConverter).toModel(orderDto);
+        verify(orderRepository).save(order);
+        assertNotNull(orderDto.getId());
+    }
+
+    @Test
+    void shouldUpgradeOrder() {
+        when(orderRepository.findById(anyInt())).thenReturn(Optional.of(order));
+        when(orderConverter.toModel(orderDto, order)).thenReturn(order);
+
+        testInstance.upgradeOrder(orderDto, ORDER_ID);
+
+        verify(orderRepository).findById(ORDER_ID);
+        verify(orderConverter).toModel(orderDto, order);
+        verify(orderRepository).save(order);
+        assertNotNull(order.getCost());
+    }
+
+    @Test
+    void shouldNotUpgradeOrder() {
+        assertThrows(NoSuchElementException.class, () -> {
+            testInstance.upgradeOrder(orderDto, ORDER_ID);
+        });
+    }
+
+    @Test
+    void shouldDropOrder() {
+        testInstance.dropOrder(ORDER_ID);
+
+        verify(orderRepository).deleteById(ORDER_ID);
+    }
+
+    @Test
+    void shouldNotDropOrder() {
+        doThrow(new RuntimeException("Exception of delete")).when(orderRepository).deleteById(ORDER_ID);
+
+        RuntimeException result = assertThrows(RuntimeException.class, () -> testInstance.dropOrder(ORDER_ID));
+
+        verify(orderRepository).deleteById(ORDER_ID);
+        assertEquals("Exception of delete", result.getMessage());
+    }
+}
---
+++ src/test/java/com/example/home_crud/Service/ProductServiceImplTest.java
@@ -0,0 +1,143 @@
+package com.example.home_crud.Service;
+
+import com.example.home_crud.Converter.ProductConverter;
+import com.example.home_crud.DTO.ProductDto;
+import com.example.home_crud.Model.Product;
+import com.example.home_crud.Repository.product.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceImplTest {
+
+    public static final int ORDER_ID = 1;
+    @InjectMocks
+    private ProductServiceImpl testInstance;
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private ProductConverter productConverter;
+    @Mock
+    private Product product;
+    @Mock
+    private List<Product> productList;
+    private ProductDto productDto;
+
+    private List<ProductDto> productDtoList;
+
+    @BeforeEach
+    public void init() {
+        productDto = new ProductDto();
+        productDto.setId(1);
+        productDto.setCost(5.44);
+        productDto.setName("Toy");
+
+        productDtoList = new ArrayList<>();
+        productDtoList.add(productDto);
+    }
+
+    @Test
+    void shouldGetProductById() {
+        when(productRepository.findById(anyInt())).thenReturn(Optional.of(product));
+        when(productConverter.productFromModel(product)).thenReturn(productDto);
+
+        ProductDto result = testInstance.getProductById(ORDER_ID);
+
+        verify(productRepository).findById(ORDER_ID);
+        verify(productConverter).productFromModel(product);
+        assertEquals(ORDER_ID, result.getId());
+        assertNotNull(result);
+    }
+
+    @Test
+    void shouldNotGetProductById() {
+        assertThrows(NoSuchElementException.class, () -> {
+            testInstance.getProductById(ORDER_ID);
+        });
+    }
+
+    @Test
+    void shouldGetProducts() {
+        when(productRepository.findAll()).thenReturn(productList);
+        when(productConverter.productFromModel(productList)).thenReturn(productDtoList);
+
+        List<ProductDto> result = testInstance.getProducts();
+
+        verify(productRepository).findAll();
+        verify(productConverter).productFromModel(productList);
+        assertEquals(productDtoList, result);
+        assertNotNull(result);
+    }
+
+    @Test
+    void shouldNotGetProducts() {
+        when(productRepository.findAll()).thenReturn(null);
+
+        List<ProductDto> result = testInstance.getProducts();
+
+        verify(productRepository).findAll();
+        assertNotNull(result);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldAddProduct() {
+        when(productConverter.productToModel(productDto)).thenReturn(product);
+
+        testInstance.addProduct(productDto);
+
+        verify(productConverter).productToModel(productDto);
+        verify(productRepository).save(product);
+        assertNotNull(product.getId());
+    }
+
+    @Test
+    void shouldUpdateProduct() {
+        when(productRepository.findById(ORDER_ID)).thenReturn(Optional.of(product));
+        when(productConverter.productToModel(productDto, product)).thenReturn(product);
+
+        testInstance.updateProduct(productDto, ORDER_ID);
+
+        verify(productRepository).findById(ORDER_ID);
+        verify(productConverter).productToModel(productDto, product);
+        verify(productRepository).save(product);
+        assertNotNull(product.getCost());
+    }
+
+    @Test
+    void shouldNotUpdateProduct() {
+        assertThrows(NoSuchElementException.class, () -> {
+            testInstance.updateProduct(productDto, ORDER_ID);
+        });
+    }
+
+    @Test
+    void shouldDropProductById() {
+        testInstance.dropProductById(ORDER_ID);
+
+        verify(productRepository).deleteById(ORDER_ID);
+    }
+
+    @Test
+    void shouldNotDropProductById() {
+        doThrow(new RuntimeException("Exception of delete")).when(productRepository).deleteById(ORDER_ID);
+
+        RuntimeException result = assertThrows(RuntimeException.class, () -> testInstance.dropProductById(ORDER_ID));
+
+        verify(productRepository).deleteById(ORDER_ID);
+        assertEquals("Exception of delete", result.getMessage());
+    }
+}
